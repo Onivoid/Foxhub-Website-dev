4fd82aa7c45a150b85b863679856f394127ba552
Logan Wilquin <lwilquin@datasolution.fr>
2019-02-14 12:30:08 +0100

    First commir Develop

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,24 +1,27 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+import logo from '../images/SkullyfoxPoly.png'
 
 const Header = (props) => (
     <header id="header" style={props.timeout ? {display: 'none'} : {}}>
         <div className="logo">
-            <span className="icon fa-diamond"></span>
+            <img src={logo} />
         </div>
         <div className="content">
             <div className="inner">
-                <h1>Dimension</h1>
-                <p>A fully responsive site template designed by <a href="https://html5up.net">HTML5 UP</a> and released<br />
-                for free under the <a href="https://html5up.net/license">Creative Commons</a> license.</p>
+                <h1>Foxhub</h1>
+                <p>
+                    Foxhub est un groupe communautaire discord de Gaming et de Développement informatique, <br/>
+                    Il est composé d'un serveur Gaming et d'un serveur de développement informatique(Web, Logiciel etc...). <br/>
+                    Pour plus d'informations et pour les liens des serveur, retrouvez ci-dessous les rubriques Pop-up descriptives.
+                </p>
             </div>
         </div>
         <nav>
             <ul>
-                <li><a href="javascript:;" onClick={() => {props.onOpenArticle('intro')}}>Intro</a></li>
-                <li><a href="javascript:;" onClick={() => {props.onOpenArticle('work')}}>Work</a></li>
-                <li><a href="javascript:;" onClick={() => {props.onOpenArticle('about')}}>About</a></li>
-                <li><a href="javascript:;" onClick={() => {props.onOpenArticle('contact')}}>Contact</a></li>
+                <li><a href="javascript:;" onClick={() => {props.onOpenArticle('common')}}>Réglement Commun</a></li>
+                <li><a href="javascript:;" onClick={() => {props.onOpenArticle('gaming')}}>Foxhub Gaming</a></li>
+                <li><a href="javascript:;" onClick={() => {props.onOpenArticle('dev')}}>Foxhub Dev</a></li>
             </ul>
         </nav>
     </header>
